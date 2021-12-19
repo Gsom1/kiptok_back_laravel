d39fdf9c2d6f2e1b133aca06f760f4d606736eb4
kip <Kip122345>
2021-12-19 18:38:05 +0200

    cookies

--- src/src/Controller/V1/VideosController.php
+++ src/src/Controller/V1/VideosController.php
@@ -6,21 +6,20 @@ use App\App\UseCases\Feeder\Feeder;
 use App\Entity\FeedCursor;
 use App\Repository\FeedCursorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
 
 class VideosController extends AbstractController
 {
     public function __construct(
         private Feeder               $feeder,
         private FeedCursorRepository $feedCursorRepository,
-        private RequestStack         $requestStack
     ) {
     }
 
-    public function getVideos()
+    public function getVideos(Request $request)
     {
-        $session = $this->requestStack->getSession();
-        $feedCursor = $session->get('feed_cursor');
+        $feedCursor = $request->cookies->get('feed_cursor');
         if ($feedCursor) {
             $feedCursor = $this->feedCursorRepository->find($feedCursor);
         }
@@ -29,8 +28,11 @@ class VideosController extends AbstractController
         }
 
         $videos = $this->feeder->getVideos($feedCursor);
-        $session->set('feed_cursor', (string)$feedCursor->getId());
 
-        return $this->json($videos);
+        $response = $this->json($videos);
+        $cookie = new Cookie('feed_cursor', (string)$feedCursor->getId());
+        $response->headers->setCookie($cookie);
+
+        return $response;
     }
 }
