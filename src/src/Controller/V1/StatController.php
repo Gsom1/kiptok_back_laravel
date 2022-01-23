40c312206b7e1c4dabe6ccef7a04f86a8bf1fbfe
Gsom1 <myreg_@list.ru>
2022-01-23 18:13:12 +0200

    users

---
+++ src/src/Controller/V1/StatController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\V1;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class StatController extends AbstractController
+{
+    public function play()
+    {
+    }
+
+    public function pause()
+    {
+
+    }
+
+    public function swipe()
+    {
+
+    }
+}
---
+++ src/src/Security/EmptyAuthenticator.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Uid\Uuid;
+
+class EmptyAuthenticator extends AbstractAuthenticator
+{
+    public function __construct(private UserRepository $userRepository, private EntityManagerInterface $em)
+    {
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return null;
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $user = null;
+        $session = $request->getSession();
+        $userId = $session->get('user_id');
+        if ($userId) {
+            $user = $this->userRepository->find($userId);
+        } else {
+            $userId = Uuid::v4();
+            $session->set('user_id', $userId);
+        }
+
+        if (!$user) {
+            $user = new User();
+            $user->setUuid($userId);
+            $user->setPassword('');
+            $this->em->persist($user);
+            $this->em->flush();
+        }
+
+        return new Passport(
+            new UserBadge($userId),
+            new PasswordCredentials(''),
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        return null;
+    }
+
+//    public function start(Request $request, AuthenticationException $authException = null): Response
+//    {
+//        /*
+//         * If you would like this class to control what happens when an anonymous user accesses a
+//         * protected page (e.g. redirect to /login), uncomment this method and make this class
+//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
+//         *
+//         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
+//         */
+//    }
+}
