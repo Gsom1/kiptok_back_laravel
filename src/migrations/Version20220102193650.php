f0fa53db323efca6469f434186e1d383352af181
Gsom1 <myreg_@list.ru>
2022-01-30 22:39:53 +0200

    migration fix

--- src/migrations/Version20220102193650.php
+++ src/migrations/Version20220102193650.php
@@ -19,7 +19,6 @@ final class Version20220102193650 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(
             'CREATE TABLE "user" (
                 id uuid PRIMARY KEY,
@@ -30,7 +29,6 @@ final class Version20220102193650 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP TABLE "user"');
     }
